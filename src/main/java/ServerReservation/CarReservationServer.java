7d938dd5e4a05caae7f5ebac96f6b7328df0d0bd
leticiaqueirooz <leticiaqueirooz@outlook.com>
2022-08-14 23:56:18 +0100

    updatefinall

--- src/main/java/ServerReservation/CarReservationServer.java
+++ src/main/java/ServerReservation/CarReservationServer.java
@@ -1,16 +1,41 @@
 package ServerReservation;
 
-import java.io.IOException;
 
+
+import carreservation.CarReservationGrpc.CarReservationImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import jmDNS.SimpleServiceRegistration;
 
-public class CarReservationServer {
-	public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(8080).addService(new CarReservationImpl()).build();
-        System.out.println("Starting server...");
-        server.start();
-        System.out.println("Server started!");
-        server.awaitTermination();
-    }
+public class CarReservationServer extends CarReservationImplBase {
+
+	public static void main(String[] args) {
+		CarReservationServer CarResevationServer = new CarReservationServer();
+
+    	System.out.println("Starting gRPC CarReservation service server.");
+    	
+    	//Define the port
+		int port = 8081;
+		
+		// jmDNS
+		String service_type = "_CarReservation._tcp.local.";
+		String service_name = "GrpcServer";
+		SimpleServiceRegistration ssr = new SimpleServiceRegistration();
+		ssr.run(port, service_type, service_name);
+
+		try {		
+			Server server = ServerBuilder.forPort(port)
+					
+					//Implementing the interface
+					
+					.addService(new CarReservationImpl()) 
+					.build()
+					.start();
+			System.out.println("Server started, listening on " + port);
+			server.awaitTermination();
+		} catch (Exception e) {
+			System.out.println(e);
+		}		
+	}
 }
+
--- src/main/java/carClient/CarClient.java
+++ src/main/java/carClient/CarClient.java
@@ -2,6 +2,7 @@ package carClient;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.jmdns.ServiceInfo;
 import javax.swing.JOptionPane;
 
 import carreservation.CarReservationGrpc;
@@ -9,11 +10,31 @@ import carreservation.UnlockCarRequest;
 import carreservation.UnlockCarResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import jmDNS.SimpleServiceDiscovery;
 
 
 public class CarClient {
      public static void main(String[] args) throws InterruptedException {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+        
+      
+     // jmdns
+     		@SuppressWarnings("unused")
+     		ServiceInfo serviceInfo;
+     		String service_type = "_CarReservation._tcp.local.";
+     		String service_name = "GrpcServer";
+     		serviceInfo = SimpleServiceDiscovery.run(service_type);
+     		
+     		// port & host
+     		int port = 8081;
+     		String host = "localhost";
+     				
+         	// build a channel
+     		ManagedChannel channel = ManagedChannelBuilder
+     				.forAddress(host , port)
+     				.usePlaintext()
+     				.build();
+        
+   
         CarReservationGrpc.CarReservationBlockingStub stub = CarReservationGrpc.newBlockingStub(channel);
         String nome = JOptionPane.showInputDialog("Please insert your Car Id/login");
         String sobrenome = JOptionPane.showInputDialog("Please insert your password");
